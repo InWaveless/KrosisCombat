2a087f3ce62e334980b2bcf2ca025fe6fd4dda4a
aquilman <rustamv.rman@gmail.com>
2024-11-02 01:44:11 +0300

    Finally managed to hook OnHit

--- include/Hooks.h
+++ include/Hooks.h
@@ -2,6 +2,8 @@
 
 namespace Hooks
 {
+    
+
     void Install() noexcept;
 
     class MainUpdate : public Singleton<MainUpdate>
@@ -15,4 +17,32 @@ namespace Hooks
 
         inline static const auto address{ target.address() };
     };
+    
+    class MeleeHit : public Singleton<MeleeHit>
+    {
+    public:
+        static void Thunk(RE::Actor* target, RE::HitData& hitData);
+
+        inline static REL::Relocation<decltype(Thunk)> func;
+
+        inline static const REL::RelocationID Actor_DealDamage{ 37673, 38627 };
+
+        inline static const auto address{ Actor_DealDamage.address() };
+    };
+
+    //class Hook_OnMeleeHit
+    //{
+    //public:
+    //    static void install()
+    //    {
+    //        REL::Relocation<uintptr_t> hook{ RELOCATION_ID(37673, 38627) }; // 140628C20       14064E760
+    //        auto&                      trampoline = SKSE::GetTrampoline();
+    //        _ProcessHit                           = trampoline.write_call<5>(hook.address() + REL::VariantOffset(0x3C0, 0x4A8, 0).offset(), processHit);
+    //        logger::info("hook:OnMeleeHit");
+    //    }
+
+    //private:
+    //    static void                                         processHit(RE::Actor* victim, RE::HitData& hitData);
+    //    inline static REL::Relocation<decltype(processHit)> _ProcessHit; // 140626400       14064BAB0
+    //};
 } // namespace Hooks
--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -7,7 +7,31 @@ namespace Hooks
         stl::write_thunk_call<MainUpdate>();
         logger::info("Installed main update hook");
         logger::info("");
+
+        // Hook_OnMeleeHit::install();
+        stl::write_thunk_call<MeleeHit>(MeleeHit::address + REL::Relocate(0x3C0, 0x4A8, 0x3C0));
+        logger::info("installed on melee hit hook");
+        logger::info("");
     }
 
     i32 MainUpdate::Thunk() noexcept { return func(); }
+
+    void MeleeHit::Thunk(RE::Actor* target, RE::HitData& hitData)
+    {
+        logger::info("target:{}, hitData:{}", target->GetName(), hitData.weapon->GetName());
+        MeleeHit::func(target, hitData);
+    }
+
+    // void Hook_OnMeleeHit::processHit(RE::Actor* victim, RE::HitData& hitData)
+    //{
+    //    using HITFLAG  = RE::HitData::Flag;
+    //    auto aggressor = hitData.aggressor.get().get();
+    //    if (!victim || !aggressor || victim->IsDead()) {
+    //        _ProcessHit(victim, hitData);
+    //        return;
+    //    }
+    //    logger::info("on melee hit hook");
+    //    //hitProcessor::GetSingleton()->processHit(aggressor, victim, hitData);
+    //    _ProcessHit(victim, hitData);
+    //}
 } // namespace Hooks
